76c7d03d7da76f68ca38a6fa0a94f128a88b6505
pkyria <pankyriakidis@outlook.com>
2025-11-19 12:12:23 +0200

    Remove cached pycache and ipynb checkpoints

--- ML/.ipynb_checkpoints/test-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-import joblib
-
-df_news = pd.read_csv('ML/WELFake_Dataset.csv')
-
-print(df_news.head(20))
